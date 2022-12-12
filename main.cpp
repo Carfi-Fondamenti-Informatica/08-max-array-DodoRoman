5559c9cf6c753390a670c919bf71bb05ea65b19a
DodoRoman <115349003+DodoRoman@users.noreply.github.com>
2022-12-12 14:40:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int dim=0;
+    cin >> dim;
+    float insieme[dim];
+    for (int i=0; i<dim; i++){
+        cin >> insieme[i];
+    }
+    cout << piu(insieme,dim) << endl;
+    return 0;
 }
