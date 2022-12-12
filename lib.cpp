254abd039d392b6c5bda29ed4b93b51ae21ba0c1
DodoRoman <115349003+DodoRoman@users.noreply.github.com>
2022-12-12 14:39:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
 
+#include "lib.h"
+
+float piu (float insiemer[], int dimensione){
+    for (int i=0; i<dimensione; i++){
+        if (insiemer[0]<insiemer[i]){
+            insiemer[0]=insiemer[i];
+        }
+        else {
+            insiemer[0]=insiemer[0];
+        }
+    }
+    return insiemer[0];
+}
